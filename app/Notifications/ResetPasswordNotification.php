7f2128ca647244c90bb01dca98d9ac01f4974854
KovalskiWeb <wesleykovalski@gmail.com>
2023-05-23 17:56:22 -0300

    Finalizado módulo 07

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    protected $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = config('app.url_frontend') . "/reset/{$this->token}";
+
+        return (new MailMessage)
+                    ->subject('Nova Senha')
+                    ->line('Você está recebendo esse e-mail porque recebemos um pedido para redefinir sua senha. Caso você não tenha feito essa solicitação, favor ignorar esta mensagem.')
+                    ->action('Atualizar Senha', $url)
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
